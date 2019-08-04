1d964dce6b76058c924c98ca6c0641b25ecf419e
Tsongtsz <damonhaoo@gmail.com>
2019-08-04 18:39:56 -0500

    Initial Commit

--- rename.py
+++ rename.py
@@ -1,1 +1,15 @@
 print('hello world!')
+import os
+
+for f in os.listdir():
+    f_name,f_ext = os.path.splitext(f)
+
+    f_title,f_course, f_num = f_name.split('_')
+
+    f_title = f_title.strip()
+    f_course = f_course.strip()
+    f_num = f_num.strip()
+
+    new_name = '{}-{}-{}'.format(f_num,f_title,f_ext)
+
+    os.rename(f,new_name)
